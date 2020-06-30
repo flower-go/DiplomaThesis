c0de0041dcda3488a46c3c45295f80dd564b1e9c
Petra Vysušilová <vysusilova.petra.m@gmail.com>
2020-06-30 21:05:35 +0200

    sentiment analysis prvni verze

--- code/pokusy_bert_sentiment/sentiment_analysis.py
+++ code/pokusy_bert_sentiment/sentiment_analysis.py
@@ -7,43 +7,45 @@ import re
 import numpy as np
 import tensorflow as tf
 import transformers
+from keras import backend as b
 
 from text_classification_dataset import TextClassificationDataset
+
 
 class Network:
     def __init__(self, args, labels):
         # TODO: Define a suitable model.
 
         # vstup
-        segments = tf.keras.layers.Input(shape=[None], dtype=tf.int32)
         subwords = tf.keras.layers.Input(shape=[None], dtype=tf.int32)
-        inp = [segments, subwords]
+        inp = [subwords]
 
-        config = transformers.BertConfig.from_pretrained(args.bert_model)
+        config = transformers.BertConfig.from_pretrained(args.bert)
         config.output_hidden_states = True
-        self.bert = transformers.TFBertModel.from_pretrained(args.bert_model,
-                                        config=config)
+        self.bert = transformers.TFBertForSequenceClassification.from_pretrained(args.bert,
+                                                             config=config)
 
-        bert_output = self.bert(subwords, attention_mask=tf.cast(subwords != 0, tf.float32))[2][-1] #vezmu posledni vrstvu
+        bert_output = self.bert(subwords, attention_mask=tf.cast(subwords != 0, tf.float32))[0]
+        # vezmu posledni vrstvu
+        print(bert_output.shape)
         # dropout
-        dropout = tf.keras.layers.Dropout(args.dropout)(bert_output)
+        # dropout = tf.keras.layers.Dropout(args.dropout)(bert_output)
         # dense s softmaxem
-        predictions = tf.keras.layers.Dense(labels, activation=tf.nn.softmax)(dropout)
+        predictions = tf.keras.layers.Dense(labels, activation=tf.nn.softmax)(bert_output)
         # model(inputs, outputs)
-        self.model = tf.keras.Model(inputs=inp, outputs = predictions)
+        self.model = tf.keras.Model(inputs=inp, outputs=predictions)
         # compile model
         self.model.compile(optimizer=tf.optimizers.Adam(),
                            loss=tf.losses.SparseCategoricalCrossentropy(),
                            metrics=[tf.metrics.SparseCategoricalAccuracy(name="accuracy")])
 
-
         self._writer = tf.summary.create_file_writer(args.logdir, flush_millis=10 * 1000)
 
-    def train_epoch(self, dataset):
-        for batch in dataset.batches:
+    def train_epoch(self, dataset, args):
+        for batch in dataset.batches(size=args.batch_size):
             metrics = self.model.train_on_batch(
-                dataset.data.tokens,
-                dataset.data.labels,
+                batch[0],
+                batch[1],
                 reset_metrics=True)
 
             tf.summary.experimental.set_step(self.model.optimizer.iterations)
@@ -53,9 +55,10 @@ class Network:
 
     def train(self, omr, args):
         # TODO: Train the network on a given dataset.
-        for e, lr in range(args.epochs):
+        for e, lr in args.epochs:
+            b.set_value(self.model.optimizer.learning_rate, lr)
             for i in range(e):
-                network.train_epoch(omr.train, args, lr)
+                network.train_epoch(omr.train, args)
                 metrics = network.evaluate(omr.dev, "dev", args)
 
         raise NotImplementedError()
@@ -80,13 +83,14 @@ if __name__ == "__main__":
     parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
     parser.add_argument("--verbose", default=False, action="store_true", help="Verbose TF logging.")
     args = parser.parse_args([] if "__file__" not in globals() else None)
-    args.epochs = [(int(epochs), float(lr)) for epochslr in args.epochs.split(",") for epochs, lr in [epochslr.split(":")]]
+    args.epochs = [(int(epochs), float(lr)) for epochslr in args.epochs.split(",") for epochs, lr in
+                   [epochslr.split(":")]]
 
     # Fix random seeds and threads
     np.random.seed(args.seed)
     tf.random.set_seed(args.seed)
-#    tf.config.threading.set_inter_op_parallelism_threads(args.threads)
-#    tf.config.threading.set_intra_op_parallelism_threads(args.threads)
+    #    tf.config.threading.set_inter_op_parallelism_threads(args.threads)
+    #    tf.config.threading.set_intra_op_parallelism_threads(args.threads)
 
     # Report only errors by default
     if not args.verbose:
@@ -100,18 +104,18 @@ if __name__ == "__main__":
     ))
 
     # TODO: Create the BERT tokenizer to `tokenizer` variable
-    tokenizer = transformers.AutoTokenizer.from_pretrained("bert-base-multilingual-uncased")
+    tokenizer = transformers.AutoTokenizer.from_pretrained(args.bert)
 
     # TODO: Load the data, using a correct `tokenizer` argument, which
     # should be a callable that given a sentence in a string produces
     # a list/np.ndarray of token integers.
-    facebook = TextClassificationDataset("czech_facebook", tokenizer=tokenizer.tokenize)
+    facebook = TextClassificationDataset("czech_facebook", tokenizer=tokenizer.encode)
 
     # Create the network and train
     network = Network(args, len(facebook.train.LABELS))
     network.train(facebook, args)
 
-    #TODO pridat vyhodnoceni na dev!!!
+    # TODO pridat vyhodnoceni na dev!!!
 
     # Generate test set annotations, but to allow parallel execution, create it
     # in in args.logdir if it exists.
