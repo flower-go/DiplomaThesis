52c95e3e199d14f004be0b11ca951592b3e8bce5
Petra Vysusilova <doubrap1@aic.aic.hide.ms.mff.cuni.cz>
2021-05-21 14:28:35 +0200

    oprava

--- code/morphodita-research/bert_finetunning_simple.py
+++ code/morphodita-research/bert_finetunning_simple.py
@@ -90,8 +90,8 @@ class Network:
             self.model.load_weights(args.bert_load)
         # compile model
         self.optimizer=tf.optimizers.Adam()
-        decay_steps = args.steps_in_epoch * (args.epochs[0][0] - args.warmup_decay)
         if args.decay_type is not None:
+            decay_steps = args.steps_in_epoch * (args.epochs[0][0] - args.warmup_decay)
             if args.decay_type == "i":
                 initial_learning_rate = args.epochs[0][1]
                 learning_rate_fn = tf.keras.optimizers.schedules.PolynomialDecay(initial_learning_rate, decay_steps,
